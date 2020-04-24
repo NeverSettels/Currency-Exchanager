9d883bc3c1eff01767935991bee8746fe39e62d5
NeverSettels <settels.eric@gmail.com>
2020-04-24 11:40:47 -0700

    create specific conversion api call and UI console.log not tested yet

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,7 +1,23 @@
 export class ExchangeApi {
-  async exchange() {
+  async exchangeRates() {
     try {
       let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+        //console.log(jsonifiedResponse);
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async specificExchange(from, to) {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/pair/${from}/${to}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
